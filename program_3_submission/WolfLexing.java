9dd1eb25bbd0c8c09d9189b6aa8e687d19788c8b
Perka Lab <dittmar@perka.com>
2016-02-23 19:55:13 -0500

    Added some headers

--- program_3_submission/WolfLexing.java
+++ program_3_submission/WolfLexing.java
@@ -1,8 +1,8 @@
 /** This class starts the lexical phase from a main method.
   * @author(Joe Alacqua)
-  * @author(Kevin Dittmar
+  * @author(Kevin Dittmar)
   * @author(William Ezekiel)
-  * @version(2/23/2016)
+  * @version(2/23/16)
   */
 package wolf;
 import wolf.lexer.*;
@@ -12,105 +12,112 @@ import java.io.*;
 class WolfLexing 
 {
     static Lexer lexer;
-	static Token token;
-	
-	// Command line format: java wolf/WolfLexing file1 file2 file3 ...
-	// If no file names are given, reads from stdin.	
-	public static void main (String[] args)
+    static Token token;
+    
+    // Command line format: java wolf/WolfLexing file1 file2 file3 ...
+    // If no file names are given, reads from stdin.
+    public static void main (String[] args)
     {
         if(args.length == 0)
         {
-			lexStdIn();
+          lexStdIn();
         }
-		else
+        else
         {
-			for(String file : args)
-            {
-				lexFile(file);
-			}
-		}	
-	}
-	
-	
-	// Do the lexical phase for WOLF from stdin, and
+          for(String file : args)
+          {
+            lexFile(file);
+  	      }
+        }
+    }
+    
+    
+    // Do the lexical phase for WOLF from stdin, and
     // send the token output to stdout.
-	private static void lexStdIn()
+    private static void lexStdIn()
     {
-		token = null;
-		lexer = new Lexer(
-            new PushbackReader(
-                new InputStreamReader(System.in), 1024
-            )
-        );
-		try
-        {
-            // Collect all of the tokens in a StringBuilder to write
-            // to the file.
-            while(!(token instanceof EOF))
-            {
-                token = lexer.next();
-                // print token
-                System.out.println(token.getClass() + ": " + token.getText());
-            }
-        }
-		catch (LexerException le)
-        {
-            System.err.println(le);
-        }
-        catch (IOException ioe) 
-        {
-            System.err.print(ioe);
-        }
-	}
+      token = null;
+      lexer = new Lexer(
+        new PushbackReader(
+          new InputStreamReader(System.in), 1024
+        )
+      );
+      // Collect all of the tokens in a StringBuilder to write
+      // to the file.
+      while((token = getToken()) != null)
+      {
+        // print token
+        System.out.println(token.getClass() + ": " + token.getText());
+      }
+    }
 
-	// Do the lexical phase for WOLF from a file,
+    // Do the lexical phase for WOLF from a file,
     // save the output to a file of the same name with the .tokens extension.
-	private static void lexFile(String filename)
+    private static void lexFile(String filename)
     {
-        token = null;
-        try
-        {
-            lexer = new Lexer(
-                new PushbackReader(
-                    new FileReader(filename), 1024
-                )
-            );
-			
-			String outputFile = filename + ".tokens";
-			File file = new File(outputFile);
+      try
+      {
+        lexer = new Lexer(
+          new PushbackReader(
+            new FileReader(filename), 1024
+          )
+      );
+      String outputFile = filename + ".tokens";
+      File file = new File(outputFile);
+      if(!file.exists())
+		  {
+        file.createNewFile();
+      }
 
-			if(!file.exists())
-            {
-				file.createNewFile();
-			}
+      FileWriter fw = new FileWriter(file.getAbsoluteFile());
+      BufferedWriter bw = new BufferedWriter(fw);
 
-			FileWriter fw = new FileWriter(file.getAbsoluteFile());
-			BufferedWriter bw = new BufferedWriter(fw);
+      StringBuilder sb = new StringBuilder();
 
-			StringBuilder sb = new StringBuilder();
+      // Collect all of the tokens in a StringBuilder to write
+      // to the file.
+      while((token = getToken()) != null)
+      {
+        sb.append(token.getClass() + ": " + token.getText() + "\n");
+      }
 
-            // Collect all of the tokens in a StringBuilder to write
-            // to the file.
-            while(!(token instanceof EOF))
-            {
-                token = lexer.next();
-                sb.append(token.getClass() + ": " + token.getText() + "\n");
-            }
+      bw.write(sb.toString());
+      bw.close();
+    }
+    catch(FileNotFoundException fnfe)
+    {
+      System.err.println(fnfe);
+    }
+    catch (IOException ioe)
+    {
+      System.err.print(ioe);
+    }
+  }
 
-			bw.write(sb.toString());
-			bw.close();
-        }
-        catch (LexerException le)
-        {
-            System.err.println(le);
-        }
-        catch(FileNotFoundException fnfe)
-        {
-            System.err.println(fnfe);
-        }
-        catch (IOException ioe)
-        {
-            System.err.print(ioe);
-        }
-	}
+  /**
+   * Gets the next Token from the input stream.
+   * @return a Token representing the next token in the input stream
+   * or null if there are no more tokens.
+   */
+  public static Token getToken()
+  {
+  	if(token instanceof EOF)
+    {
+  		return null;
+  	}
+  	try
+    {
+  		return lexer.next();
+  	}	
+  	catch (LexerException le)
+    {
+  		System.err.println(le);
+  		return null;
+  	}
+  	catch (IOException ie)
+    {
+  		System.err.println(ie);
+  		return null;
+  	}
+  }
 }
